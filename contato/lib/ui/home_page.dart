20ff55fda4c7dec6a5e83908b67561f7a701e09f
Rodrigo Lopes de Aquino <rodrigolaquino@gmail.com>
2020-04-24 14:11:52 -0400

    estudos

--- contato/lib/ui/home_page.dart
+++ contato/lib/ui/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:contato/helpers/contact_helper.dart';
+import 'package:contato/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,29 +12,93 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   ContactHelper helper = ContactHelper();
 
+  List<Contact> contacts = List();
+
   @override
   void initState() {
     super.initState();
 
-    // Contact c = Contact();
-    // c.name = "Rodrigo Lopes";
-    // c.email = "rodrigolaquino@gmail.com";
-    // c.phone = "555555";
-    // c.img = "img";
-
-    // helper.saveContact(c);
-    // helper.getContact(1).then((contact) {
-    //   contact.name = "Rodrigo Lopes";
-    //   helper.updateContact(contact).then((contact) {
-    //     helper.getContact(1).then((contact) {
-    //       print(contact);
-    //     });
-    //   });
-    // });
+    helper.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contatos'),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.red,
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          return _contactCard(context, contacts[index]);
+        },
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, Contact contact) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 80.0,
+                  height: 80.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: contact.img != null
+                            ? FileImage(File(contact.img))
+                            : AssetImage('images/person.png')),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        contact.name ?? "",
+                        style: TextStyle(
+                            fontSize: 22.0, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        contact.email ?? "",
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      Text(
+                        contact.phone ?? "",
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )),
+      ),
+    );
+  }
+
+  void _showContactPage({Contact contact}) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => ContactPage(
+                  contact: contact,
+                )));
   }
 }
